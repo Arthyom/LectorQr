973ec23fd38629418cd086d952425c6fcd2d725b
arthyom <allpipiasaaa@hotmail.com>
2016-09-23 12:32:56 -0500

    Subiendo proyecto codigo Qr

---
+++ GenerarCodigoQt/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace GenerarCodigoQt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 254);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 273);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 299);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(246, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(265, 41);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(324, 278);
+            this.panel2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(265, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(324, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 325);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(430, 325);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 43);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(596, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(132, 199);
+            this.listBox1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(596, 219);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(132, 149);
+            this.panel3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 373);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ GenerarCodigoQt/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System.IO;
+using AForge.Video.DirectShow;
+using AForge.Video;
+using MySql.Data.MySqlClient;
+using AForge.Controls;
+using BarcodeLib.BarcodeReader;
+// el primer paso es enlazar la dll del conector que el paquete msi, agrego al sistema, hecho esto solo es necesario agregar el espacio de nombres
+
+
+namespace GenerarCodigoQt
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection ColeccionDisp;
+
+        void ConectarConMysql()
+        {
+            // crear cadena de conexion, se puede hacer a mano o creando un connectionbuilder
+            MySqlConnection Conexion = new MySqlConnection("Server = localhost; Database=qr; Uid=root; Pwd= ;");
+
+            // crear un comando para la base de datos
+            MySqlCommand Comando = new MySqlCommand("INSERT INTO usuario( carrera ) VALUES ('victor');",Conexion);
+
+            // abrir la conexion con la base de datos para ejecutar el comando 
+            Conexion.Open();
+
+            int a = 2, c = 43;
+
+            if ( Convert.ToBoolean(Comando.ExecuteNonQuery() ) )
+                 a = 2;
+            else
+                 c = 3;
+
+           
+
+            Conexion.Close();
+        }
+        
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Generar";
+            button2.Text = "Insertar";
+            button3.Text = "Detener";
+            button4.Text = "Iniciar";
+
+            // agregar control
+            VideoSourcePlayer ControlVideo = new VideoSourcePlayer();
+            ControlVideo.BackColor = Color.Red;
+            ControlVideo.Size = new Size(panel2.Width,panel2.Height);
+            ControlVideo.Name = "ControlVideo";
+            panel2.Controls.Add(ControlVideo);
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /// generar un codigo QR para los elementos de la caja 
+
+            // crear un encoder, codificador
+            QrEncoder Codificador = new QrEncoder( ErrorCorrectionLevel.H );
+
+            // crear un codigo QR
+            QrCode Codigo = new QrCode();
+
+            // generar generar  un codigo apartir de datos, y pasar el codigo por referencia
+            Codificador.TryEncode(textBox1.Text, out Codigo);
+
+            // generar un graficador 
+            GraphicsRenderer Renderisado = new GraphicsRenderer(new FixedCodeSize(200, QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+
+            // generar un flujo de datos 
+            MemoryStream ms = new MemoryStream();
+
+            // escribir datos en el renderizado
+            Renderisado.WriteToStream(Codigo.Matrix, ImageFormat.Png, ms);
+
+            // generar controles para ponerlos en el form
+            var ImagenQR = new Bitmap(ms);
+            var ImgenSalida = new Bitmap(ImagenQR, new Size(panel1.Width, panel1.Height));
+
+            // asignar la imagen al panel 
+            panel1.BackgroundImage = ImgenSalida;
+            
+
+
+                 
+        }
+
+        // insertar en la base de datos
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random Generador = new Random();
+            // generar 6 numeros aleatorios 
+            string NumeroId = "";
+
+            for (int i = 0; i < 6; i++)
+                NumeroId += Generador.Next(0, 10).ToString();
+
+            MySqlConnection Con = new MySqlConnection("Server=localhost; Database=codigoqr; Uid=root; Pwd=;");
+
+            MySqlCommand Com = new MySqlCommand("INSERT INTO Usuarios (IdUsuario, nombre, carrera, asistencia) VALUES (" + " ' "+  Convert.ToUInt32( NumeroId) + " ' " + "," + " ' " + textBox1.Text + " ' "+ "," +" ' "+ textBox2.Text + " ' "+","+ 0 +");", Con);
+            MySqlCommand Com2 = new MySqlCommand("SELECT * FROM usuarios", Con);
+
+            Con.Open();
+
+
+            if ( Convert.ToBoolean( Com.ExecuteNonQuery()))
+            {
+                MessageBox.Show("Insertado");
+        
+            }
+            Con.Close();
+
+
+            Con.Open();
+            // generar un lector 
+            MySqlDataReader Lector = Com2.ExecuteReader();
+
+            //leer toda la consulta 
+            while (Lector.Read())
+                MessageBox.Show(Lector.GetString(0) + Lector.GetString(1) + Lector.GetString(2));
+
+
+            Con.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+            // conseguir todos los dispositivos de video
+            this.ColeccionDisp = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            // desplegar dispositivos en el combo
+            foreach (FilterInfo disp in ColeccionDisp)
+                comboBox1.Items.Add(disp.Name);
+
+            comboBox1.SelectedIndex = 0;
+
+            // conseguir el control de video del form
+            VideoSourcePlayer Reproductor = (VideoSourcePlayer)this.panel2.Controls["ControlVideo"];
+
+           Reproductor.VideoSource = new VideoCaptureDevice(this.ColeccionDisp[comboBox1.SelectedIndex].MonikerString);
+            Reproductor.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            VideoSourcePlayer Reproductor = (VideoSourcePlayer)this.panel2.Controls["ControlVideo"];
+            Reproductor.SignalToStop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            VideoSourcePlayer Reproductor = (VideoSourcePlayer)this.panel2.Controls["ControlVideo"];
+
+            // verificar que se leea algo
+            if (Reproductor.GetCurrentVideoFrame() != null)
+            {
+                // conseguir la imagen de la  web cam
+                Bitmap imagen = new Bitmap(Reproductor.GetCurrentVideoFrame());
+
+                // conseguir el resultado de la lectura 
+             string[] codigo = BarcodeReader.read(imagen, BarcodeReader.QRCODE);
+
+                // limpiar la memoria
+                imagen.Dispose();
+
+                // agregar cuando se lea algo
+                if (codigo != null /*&& codigo.Count() > 0*/)
+                {
+                    listBox1.Items.Add(codigo[0]);
+
+                    // verificar si existe coincidencia en la base de datos para "codigo"
+
+                    /* crear una nueva conexion */
+                   // MySqlConnection Conexion = new MySqlConnection("server=loclahost; database=codigoqr; uid=root ; pwd=;");
+
+                    /* crear un comando */
+                    //MySqlCommand Comando = new MySqlCommand("SELECT * FROM Usuarios WHERE Idusuario = " + " ' " + codigo + " ';", Conexion);
+
+                    /* abrir la conexion y ejecutar el comando */
+                    //MySqlDataReader Lector = Comando.ExecuteReader();
+
+                   /* if (Lector != null)
+                        panel3.BackColor = Color.Green;
+                    else
+                        panel3.BackColor = Color.Red;*/
+                }
+                else
+                    panel3.BackColor = Color.Black;
+
+                   
+            }
+        }
+    }
+}
